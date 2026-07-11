2ff4a741cc0524308c461ee4f43eab16169e20c7
Yubraj Karki <yubrajkarki188@gmail.com>
2026-07-12 01:12:46 +0545

    new commit

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -1,8 +1,14 @@
-import type { NextConfig } from 'next';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const backendUrl = process.env.BACKEND_URL || 'http://localhost:4000';
 
-const nextConfig: NextConfig = {
+const nextConfig = {
+  turbopack: {
+    root: __dirname
+  },
   async rewrites() {
     return [
       {
